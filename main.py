e1b030631d71870e654c4b807ce0e94db26c5d8c
Lang Li <langli1307@gmail.com>
2021-01-26 20:34:09 -0800

    Update main.py
    Adjusting name properly now

--- main.py
+++ main.py
@@ -26,15 +26,15 @@ async def on_message(message):
                 if not member.bot and not str(member.top_role) == 'PimpDaddy':
                     print(member.name + " is not a bot")
                     print(member.name + " has " + str(member.top_role) + " role")
-                    print("Adding Zen to " + member.name + "\'s nickname")
-                    if not member.nick.lower().count("zen") > 0:
+                    
+                    if not member.nick.lower().count("zen") > 0 or not member.nick.count(member.name) > 0:
+                        print("Adding Zen to " + member.name + "\'s nickname")
                         await member.edit(nick="Zen " + member.name)
                 
                 
                 
             
-                    
-
+             
 @client.event
 async def on_member_join(member):
     await member.edit(nick="Zen " + member.name)
